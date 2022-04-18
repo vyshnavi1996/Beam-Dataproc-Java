c3419a98a5b4a1ebc8b5c733360bfb4dde947481
himaja33 <42191794+himaja33@users.noreply.github.com>
2022-04-18 13:17:47 -0500

    updated

---
+++ Himaja/java-word-count-beam-master/src/main/java/org/apache/beam/examples/MinimalPageRankParachuri.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.examples;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Filter;
+import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.TypeDescriptors;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionList;
+
+// created a class name MinimalPageRankParachuri
+public class MinimalPageRankParachuri {
+  public static void main(String[] args) {
+	  
+   
+    PipelineOptions options = PipelineOptionsFactory.create();    //created a PipelineOptions object
+    Pipeline p = Pipeline.create(options);
+    String dataFolder = "web04";
+   PCollection<KV<String,String>> p1 = ParachuriMapper01(p,"go.md",dataFolder);
+   PCollection<KV<String,String>> p2 = ParachuriMapper01(p,"python.md",dataFolder);
+   PCollection<KV<String,String>> p3 = ParachuriMapper01(p,"java.md",dataFolder);
+   PCollection<KV<String,String>> p4 = ParachuriMapper01(p,"README.md",dataFolder);
+   //merged all the key value pairs in to the PCollectionList and then in to the Pcollection
+    PCollectionList<KV<String, String>> pCollectionList = PCollectionList.of(p1).and(p2).and(p3).and(p4);
+    PCollection<KV<String, String>> mergedList = pCollectionList.apply(Flatten.<KV<String,String>>pCollections());
+    PCollection<String> pLinksString = mergedList.apply(MapElements.into(TypeDescriptors.strings()).via((mergeOut)->mergeOut.toString()));
+    pLinksString.apply(TextIO.write().to("Parachuricounts"));  // generating the output with the file name Parachuricounts
+    p.run().waitUntilFinish();
+  }
+ public static PCollection<KV<String,String>> ParachuriMapper01(Pipeline p, String filename, String dataFolder){
+    String newdataPath = dataFolder + "/" + filename;
+     PCollection<String> pcolInput = p.apply(TextIO.read().from(newdataPath));
+     PCollection<String> pcollinkLines = pcolInput.apply(Filter.by((String line) -> line.startsWith("[")));
+     PCollection<String> pcolLinks = pcollinkLines.apply(MapElements.into((TypeDescriptors.strings()))
+     .via((String linkLine) ->linkLine.substring(linkLine.indexOf("(")+1, linkLine.length()-1)));
+     PCollection<KV<String,String>> pColKVPairs =  pcolLinks.apply(MapElements.into(TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.strings()))
+     .via((String outLink) -> KV.of(filename,outLink)));
+    return pColKVPairs;
+  }
+
+}
+ 
